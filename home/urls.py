ac736ff63b2cbff121d2f61834bd44466a840876
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-04-24 13:30:15 -0400

    Added js for getting data dynamically

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import home, stream_data, my_view, astreaming
 
 urlpatterns = [
     path('', home, name='home'),
+    path('stream-data/', stream_data, name='stream_data'),
+    path('my_view/', my_view, name='my_view'),
+    path('astreaming/', astreaming, name='astreaming'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ import schedule
 import threading
 import time
 import json
-from django.http import StreamingHttpResponse
+import asyncio
+from django.http import StreamingHttpResponse, JsonResponse
 from .models import TestModel
 # Create your views here.
 
@@ -16,12 +17,51 @@ def home(request):
     }
     return render(request, template_name, context)
 
-def get_data_dynamically(request):
-      def get_object():
+def stream_data(request):
+    def stream_data_generator():
         queryset = TestModel.objects.all()
-        for item in queryset.iterator():
-             yield json.dumps({'number':item.number})
-      return StreamingHttpResponse(get_object(),content_type='application/json')
+        data = []
+        for obj in queryset:
+            id = obj.id
+            number = str(obj.number)
+            data.append({'id': id, 'number': number})
+            # json_data = json.dumps(data)
+            # # print('ok ==>', obj.number)
+            # yield json_data
+        json_data = json.dumps(data)
+        yield json_data
+        # data = list(TestModel.objects.values())
+        # data = JsonResponse(data, safe=False)
+        # yield data
+
+    # response = StreamingHttpResponse(stream_data_generator(), content_type='text/event-stream')
+    response = StreamingHttpResponse(stream_data_generator(), content_type="application/json")
+    return response
+
+def my_view(request):
+    def generate_data():
+        for i in range(100000):
+            print('data==========> ', i)
+            # time.sleep(10)
+            yield f"Chunk {i}\n"  # Replace with your data generation logic
+
+    response = StreamingHttpResponse(generate_data(), content_type="text/plain")
+    return response
+
+def astreaming(_):
+    async def stream():
+        for i in range(10):
+            await asyncio.sleep(5)
+            yield f"Chunk: {i}\n".encode()
+
+    return StreamingHttpResponse(stream())
+
+# def get_data_dynamically(request):
+#       def get_object():
+#         queryset = TestModel.objects.all()
+#         for item in queryset.iterator():
+#              yield json.dumps({'number':item.number})
+#         return StreamingHttpResponse(get_object(),content_type='application/json')
 
 def save_test_model():
     num = random.random()
@@ -36,6 +76,6 @@ def run_scheduler():
 		schedule.run_pending()
 		time.sleep(1)
 
-schedule.every(1).minutes.do(save_test_model)
+# schedule.every(1).minutes.do(save_test_model)
 threading.Thread(target=run_scheduler).start()
 # ================== end schedular ================
